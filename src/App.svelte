942c914577884c24a99bd57a9a3cb34179fd7763
András Morauszki <morauszki.andras@gmail.com>
2025-11-20 16:32:38 +0100

    Display component: hardcoded

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,22 @@
 <script lang="ts">
+  import Display from "./components/Display.svelte";
 </script>
 
 <main>
-  <div>
-    <h1>Password Generator App</h1>
-  </div>
+  <h1>Password Generator</h1>
+  <Display />
 </main>
 
 <style>
+  main {
+    width: 100%;
+  }
+
+  h1 {
+    font-size: 1rem;
+    color: var(--text-grey);
+    font-weight: 700;
+    text-align: center;
+    margin-bottom: 2rem;
+  }
 </style>
---
+++ src/components/Display.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import copyImgUrl from "../assets/images/icon-copy.svg";
+</script>
+
+<section>
+  <p>P4$5W0rD!</p>
+  <img src={copyImgUrl} alt="Copy to Clipboard" />
+</section>
+
+<style scoped>
+  section {
+    width: 100%;
+    background-color: var(--container);
+    padding: 1rem 1.125rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  p {
+    color: var(--placeholder);
+    font-size: 1.5rem;
+    font-weight: 700;
+  }
+
+  img {
+    height: 1.25rem;
+  }
+</style>
